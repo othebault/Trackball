431ae50907c5c482f37136e8d8ae4f73aa68aae6
othebault <olivier@othebau.lt>
2014-04-08 21:36:17 +0200

    Création

---
+++ README.md
@@ -0,0 +1,34 @@
+Trackball
+=========
+
+Introduction
+------------
+
+L'objectif de cette librairie est de fournir des fonctions de detection de balles. 
+
+Prérequis
+---------
+
+SimpleCV 1.3
+windows vista ou +
+
+Licence
+-------
+
+"Trackball" est diffusé suivant les termes de la Licence Publique Générale GNU (GPL) version 3.0.
+
+Pour toute question ou commentaire sur cette application, vous pouvez contacter le développeur du projet:
+
+Olivier THEBAULT (olivier@othebau.lt)
+
+Versions
+--------
+
+V 0.1 : Algo de base pour la détection de balle
+
+Roadmap
+-------
+
+ - Détecter la balle la plus proche
+ - Calculer sa position 
+
---
+++ trackball3.py
@@ -0,0 +1,33 @@
+# -*- coding: cp1252 -*-
+import SimpleCV
+from SimpleCV import Image
+ 
+display = SimpleCV.Display()
+
+img = Image("ping/source/image3.jpeg")
+
+# parametre pour image1 : (dilate=2), (stretch(200,255), isCircle(0.2)
+# parametre pour image2 : non trouvé
+# parametre pour image3 : (dilate=2), (stretch(2,120), isCircle(0.2)
+# parametre pour image4 : (dilate=2), (stretch(2,120), isCircle(0.2)
+# parametre pour image5 : (dilate=2), (stretch(2,120), isCircle(0.2)
+# parametre pour image6 : plante
+# parametre pour image7 : non trouvé
+            
+dist = img.colorDistance(SimpleCV.Color.ORANGE).dilate(2)
+
+segmented = dist.stretch(2,120).invert()
+
+blobs = segmented.findBlobs()
+
+if blobs:
+
+        balles = blobs.filter([b.isCircle(0.2) for b in blobs])
+                
+        if balles:
+                for balle in balles:
+                        img.drawCircle((balle.x,balle.y),balle.radius(),SimpleCV.Color.BLUE,3 )
+
+img.show()
+
+
